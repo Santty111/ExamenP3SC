4e0e95f2fafb157bcb735c481d37f3a583c5d7a3
Santty <santtyconla@gmail.com>
2025-01-20 09:57:27 -0500

    commit 1 Creacion de carpetas y clase marvel characters

---
+++ Model/MarvelCharactersc.cs
@@ -0,0 +1,27 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenP3SC.Model
+{
+    public class MarvelCharactersc
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Thumbnail { get; set; }
+        public string DetailUrl { get; set; }
+    }
+
+    public class LocalMarvelCharacter
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Thumbnail { get; set; }
+    }
+
+}
